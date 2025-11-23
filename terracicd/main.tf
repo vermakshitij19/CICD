f3ae6cdbbb34dfb0097f86e625b74204104c8e20
Kshitij <vermakshitij19@gmail.com>
2025-11-23 12:23:42 +0530

    cicd

---
+++ terracicd/main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "dev" {
+    ami = "ami-0f88e80871fd81e91"
+    instance_type = "t2.micro"
+    tags = {
+      Name = "vvvnaresh"
+    }
+}
