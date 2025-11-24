a681b57a06b50f91069a73c14eb8ec79ce09c5e0
vermakshitij19 <34025947+vermakshitij19@users.noreply.github.com>
2025-11-25 01:29:44 +0530

    Add service account impersonation to provider
    Added impersonation for service account in provider configuration.

--- terracicd/main.tf
+++ terracicd/main.tf
@@ -8,30 +8,9 @@ terraform {
 }
 
 provider "google" {
-  project = "project-fe7d28ee-6aa8-4b6f-88b"
-  region  = "us-central1"
-  zone    = "us-central1-a"
-  # Using gcloud ADC (no service account JSON required)
-}
+  project     = "project-fe7d28ee-6aa8-4b6f-88b"
+  region      = "us-central1"
+  zone        = "us-central1-a"
 
-resource "google_compute_instance" "dev" {
-  name         = "vm-from-jenkins"
-  machine_type = "e2-micro"
-  zone         = "us-central1-a"
-
-  boot_disk {
-    initialize_params {
-      image = "projects/debian-cloud/global/images/family/debian-12"
-    }
-  }
-
-  network_interface {
-    network = "default"
-
-    access_config {}
-  }
-
-  labels = {
-    env = "dev"
-  }
+  impersonate_service_account = "terraform-sa@project-fe7d28ee-6aa8-4b6f-88b.iam.gserviceaccount.com"
 }
