e8b0219fe51624a645c6747cd3b2e525b0d49261
vermakshitij19 <34025947+vermakshitij19@users.noreply.github.com>
2025-11-25 01:36:57 +0530

    Add Google Compute Instance resource configuration

--- terracicd/main.tf
+++ terracicd/main.tf
@@ -11,6 +11,23 @@ provider "google" {
   project     = "project-fe7d28ee-6aa8-4b6f-88b"
   region      = "us-central1"
   zone        = "us-central1-a"
-
+  
   impersonate_service_account = "terraform-sa@project-fe7d28ee-6aa8-4b6f-88b.iam.gserviceaccount.com"
+}
+
+resource "google_compute_instance" "dev" {
+  name         = "jenkins-tf-vm"
+  machine_type = "e2-micro"
+  zone         = "us-central1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {}
+  }
 }
